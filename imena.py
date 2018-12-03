686b7abc78957f5fd00774b39625cfe3bb7e9579
Александр Галицкий <linuxforse@gmail.com>
2018-12-03 16:13:15 +0200

    Добавлена возможность генерировать мужские или женские пары имя:фамилия используя при вызове программы аргумент -f или -m

--- imena.py
+++ imena.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
 import random
+mansi = "imena_m.txt"
+mansf = "family_m.txt"
+womani = "imena_f.txt"
+womanf = "family_f.txt"
 
-def imfaot():
-	f = open("imena_m.txt","r")
+
+def imfaot(ci,cf):
+
+	f = open(ci,"r")
 	line = f.readlines()
 	lenline = len(line)
 	rand = random.randint(0,lenline)
 	f.close()
 
-	r = open("family_m.txt","r")
+	r = open(cf,"r")
 	fam = r.readlines()
 	lenfam = len(fam)
 	famrand = random.randint(0,lenfam)
@@ -24,7 +31,18 @@ def imfaot():
 	b = b.capitalize()
 	return (a,b)
 
-imret, famret = imfaot()
+if len(sys.argv) <= 1:
+	print("Введите -m или -f")
+elif sys.argv[1] == "-m":
+	ci = mansi
+	cf = mansf
+elif sys.argv[1] == "-f":
+	ci = womani
+	cf = womanf
+else:
+	print("Неправильный аргумент командной строки")
+
+imret, famret = imfaot(ci,cf)
 print(imret)
 print(famret)
 
